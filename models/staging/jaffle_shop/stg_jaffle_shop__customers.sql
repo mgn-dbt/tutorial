42cbce85194e9615ce924481f280e377fc005a93
Marc Guedon <marc.guedon@cgi.com>
2026-01-07 14:46:16 +0000

    unit tests

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,8 +1,14 @@
     select
         id as customer_id,
         first_name,
-        last_name
+        last_name,
+        upper(left(first_name, 1)) 
+            || lower(right(first_name, length(first_name) -1)) 
+            || ' '
+            || upper(left(last_name, 1)) 
+            || lower(right(last_name, length(last_name) -1)) 
+        as full_name
 
     from {{ source('jaffle_shop', 'customers') }}
-    --`dbt-tutorial`.jaffle_shop.customers
+
     
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -2,7 +2,12 @@
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
+        date_diff( date({{ dbt.current_timestamp() }}), order_date, DAY ) as days_since_ordered,
+        case 
+            when status like '%shipped%' then 'shipped'
+            when status like '%return%' then 'returned'
+            when status like '%pending%' then 'placed'
+            else status
+        end as status
 
     from {{ source('jaffle_shop', 'orders') }}
-    --`dbt-tutorial`.jaffle_shop.orders
