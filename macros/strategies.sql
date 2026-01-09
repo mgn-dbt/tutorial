3429fb90936795bf408165244b1b61d552178b6a
GUEDON Marc <marc.guedon@cgi.com>
2026-01-09 15:04:20 +0100

    snapshot check corrige

--- macros/strategies.sql
+++ macros/strategies.sql
@@ -1,6 +1,55 @@
+{% macro snapshot_check_all_get_existing_columns(node, target_exists, check_cols_config) -%}
+    {%- if not target_exists -%}
+        {#-- no table yet -> return whatever the query does --#}
+        {{ return((false, query_columns)) }}
+    {%- endif -%}
+
+    {#-- handle any schema changes --#}
+    {%- set target_relation = adapter.get_relation(database=node.database, schema=node.schema, identifier=node.alias) -%}
+
+    {% if check_cols_config == 'all' %}
+        {%- set query_columns = get_columns_in_query(node['compiled_code']) -%}
+
+    {% elif check_cols_config is iterable and (check_cols_config | length) > 0 %}
+        {#-- query for proper casing/quoting, to support comparison below --#}
+        {%- set select_check_cols_from_target -%}
+            {#-- N.B. The whitespace below is necessary to avoid edge case issue with comments --#}
+            {#-- See: https://github.com/dbt-labs/dbt-core/issues/6781 --#}
+            select {{ check_cols_config | join(', ') }} from (
+                {{ node['compiled_code'] }}
+            ) subq
+        {%- endset -%}
+        {% set query_columns = get_columns_in_query(select_check_cols_from_target) %}
+
+    {% else %}
+        {% do exceptions.raise_compiler_error("Invalid value for 'check_cols': " ~ check_cols_config) %}
+    {% endif %}
+
+    {{ log("MOI1=" ~ query_columns, True) }}
+
+    {%- set existing_cols = adapter.get_columns_in_relation(target_relation) | map(attribute = 'name') | list -%}
+    {%- set ns = namespace() -%} {#-- handle for-loop scoping with a namespace --#}
+    {%- set ns.column_added = false -%}
+
+    {{ log("MOI2=" ~ existing_cols, True) }}
+
+    {%- set intersection = [] -%}
+    {%- for col in query_columns -%}
+        {%- if col in existing_cols -%}
+            {%- do intersection.append(adapter.quote(col)) -%}
+        {%- else -%}
+            {% set ns.column_added = true %}
+        {%- endif -%}
+    {%- endfor -%}
+
+    {{ log("MOI3=" ~ intersection, True) }}
+
+    {{ return((ns.column_added, intersection)) }}
+{%- endmacro %}
+
 
 -- funcsign: (model, string, string, config, bool) -> struct{unique_key: list[string]|string|none, updated_at: string, row_changed: string, scd_id: string, invalidate_hard_deletes: bool, hard_deletes: string}
-{% macro default__snapshot_check_strategy(node, snapshotted_rel, current_rel, model_config, target_exists) %}
+{% macro snapshot_check_strategy(node, snapshotted_rel, current_rel, model_config, target_exists) %}
     {# The model_config parameter is no longer used, but is passed in anyway for compatibility. #}
     {% set check_cols_config = config.get('check_cols') %}
     {% set primary_key = config.get('unique_key') %}
@@ -31,7 +80,7 @@
     )
     {%- endset %}
 
-    {% set scd_args = api.Relation.scd_args(primary_key, check_cols) %} -- noqa: primary_key is a string|list[string]|none
+    {% set scd_args = api.Relation.scd_args(primary_key, updated_at) %} -- noqa: primary_key is a string|list[string]|none
     {% set scd_id_expr = snapshot_hash_arguments(scd_args) %}
 
     {% do return({
