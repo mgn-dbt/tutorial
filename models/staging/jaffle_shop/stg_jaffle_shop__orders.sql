2e13c95e2f9ffd9607815243ac85dabeea9d47c1
GUEDON Marc <marc.guedon@cgi.com>
2026-01-16 14:01:59 +0100

    essais

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,3 +1,6 @@
+--{%- set cejour = dbt.current_timestamp() -%}
+--'"' ~   ~ '"'
+
 with 
 source as (
     select * from {{ source('jaffle_shop', 'orders') }}
@@ -8,7 +11,15 @@ transformed as (
         id as order_id,
         user_id as customer_id,
         order_date,
-        date_diff( date({{ my_current_timestamp() }}), order_date, DAY ) as days_since_ordered,
+/*
+        case 
+            when status not in ('returned','return_pending') 
+            then order_date 
+        end as order_placed_at,
+*/
+        -- {##{ datediff("order_date", cejour, "day") }##} as days_since_ordered,
+        {{ datediff("order_date", "CURRENT_DATETIME()", "day") }} as days_since_ordered,
+        -- date_diff( date({{ my_current_timestamp() }}), order_date, DAY ) as days_since_ordered,
         case 
             when status like '%shipped%' then 'shipped'
             when status like '%return%' then 'returned'
