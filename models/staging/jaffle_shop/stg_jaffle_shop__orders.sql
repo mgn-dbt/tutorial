7d7409d46ec156235adf3209c3ecb8e8d81250b3
GUEDON Marc <marc.guedon@cgi.com>
2026-01-19 15:57:14 +0100

    added sqlfluff

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,4 +1,4 @@
-with 
+with
 source as (
     select * from {{ source('jaffle_shop', 'orders') }}
 ),
@@ -9,16 +9,16 @@ transformed as (
         user_id as customer_id,
         order_date,
 
-        case 
-            when status not in ('returned','return_pending') 
-            then order_date 
+        case
+            when status not in ('returned', 'return_pending')
+                then order_date
         end as order_placed_at,
 
         --fonctionne avec "CURRENT_DATE()"
         {%- set cejour = dbt.current_timestamp() -%}
         {{ datediff("order_date", cejour, "day") }} as days_since_ordered,
-        
-        case 
+
+        case
             when status like '%shipped%' then 'shipped'
             when status like '%return%' then 'returned'
             when status like '%pending%' then 'placed'
