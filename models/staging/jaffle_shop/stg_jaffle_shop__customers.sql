fddf1eb846235875e1eeccca55bbc042a3d65fbd
GUEDON Marc <marc.guedon@cgi.com>
2026-01-21 11:42:57 +0100

    initcap

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -8,12 +8,14 @@ transformed as (
         id as customer_id,
         last_name, --surname,
         first_name, --givenname,
-        upper(left(first_name, 1))
-        || lower(right(first_name, length(first_name) - 1))
-        || ' '
-        || upper(left(last_name, 1))
-        || lower(right(last_name, length(last_name) - 1))
-            as full_name
+
+        -- upper(left(first_name, 1))
+        -- || lower(right(first_name, length(first_name) - 1))
+        -- || ' '
+        -- || upper(left(last_name, 1))
+        -- || lower(right(last_name, length(last_name) - 1)) 
+
+        initcap({{ dbt.concat(["first_name", "' '", "last_name"]) }}) as full_name
 
     from source
 )
