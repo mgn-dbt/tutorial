af523dcb61e32d730d68bf3bd9f54281197e3a0b
Marc Guedon <marc.guedon@cgi.com>
2026-01-21 09:36:52 +0000

    sqlfluff vs sqlfmt

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,29 +1,30 @@
 with
-source as (
-    select * from {{ source('jaffle_shop', 'orders') }}
-),
+    source as (select * from {{ source("jaffle_shop", "orders") }}),
 
-transformed as (
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
+    transformed as (
+        select
+            id as order_id,
+            user_id as customer_id,
+            order_date,
 
-        case
-            when status not in ('returned', 'return_pending')
-                then order_date
-        end as order_placed_at,
+            case
+                when status not in ('returned', 'return_pending') then order_date
+            end as order_placed_at,
 
-        {{ datediff("order_date", my_current_timestamp(), "day") }} 
+            {{ dbt.datediff("order_date", my_current_timestamp(), "day") }}
             as days_since_ordered,
 
-        case
-            when status like '%shipped%' then 'shipped'
-            when status like '%return%' then 'returned'
-            when status like '%pending%' then 'placed'
-            else status
-        end as order_status
-    from source
-)
+            case
+                when status like '%shipped%'
+                then 'shipped'
+                when status like '%return%'
+                then 'returned'
+                when status like '%pending%'
+                then 'placed'
+                else status
+            end as order_status
+        from source
+    )
 
-select * from transformed
+select *
+from transformed
