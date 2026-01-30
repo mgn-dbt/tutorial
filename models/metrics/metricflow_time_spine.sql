c758492c2ce06186c766ff6ba23a9ac3dfd1c3d4
GUEDON Marc <marc.guedon@cgi.com>
2026-01-30 10:33:56 +0100

    changements

--- models/metrics/metricflow_time_spine.sql
+++ models/metrics/metricflow_time_spine.sql
@@ -1,9 +1,10 @@
 {{ 
     config (
-        static_analysis='unsafe',
+        materialized = 'table',
     )
 }}
 
+--         static_analysis='unsafe',
 --dbt1000: Detected unsafe introspection which may lead to non-deterministic static analysis. 
 --To suppress this warning, set static_analysis to 'unsafe' in the nodes' configuration. 
 --Learn more: https://docs.getdbt.com/docs/fusion/new-concepts. 
@@ -12,13 +13,21 @@
 with
 days as (
     {{ 
-        dbt_utils.date_spine(
+        dbt.date_spine(
             datepart="day",
-            start_date="cast('2025-01-01' as date)",
+            start_date="cast('2000-01-01' as date)",
             end_date="cast('2030-01-01' as date)"
         )
     }}
+),
+
+final as (
+    select cast(date_day as date) as date_day
+    from days
 )
 
-select cast(date_day as date) as date_day
-from days
+select *
+from final
+where
+    date_day > {{ dbt.dateadd("year", -5, current_timestamp()) }}  -- Keep recent dates only
+    and date_day < {{ dbt.dateadd("day", 30, current_timestamp()) }}
