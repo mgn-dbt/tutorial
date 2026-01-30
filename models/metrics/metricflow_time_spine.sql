15464ba745903afce00c6186cfc445452fd018ca
Marc Guedon <marc.guedon@cgi.com>
2026-01-30 09:49:13 +0000

    error / Error

--- models/metrics/metricflow_time_spine.sql
+++ models/metrics/metricflow_time_spine.sql
@@ -4,12 +4,6 @@
     )
 }}
 
---         static_analysis='unsafe',
---dbt1000: Detected unsafe introspection which may lead to non-deterministic static analysis. 
---To suppress this warning, set static_analysis to 'unsafe' in the nodes' configuration. 
---Learn more: https://docs.getdbt.com/docs/fusion/new-concepts. 
---Nodes: 'model.jaffle_shop.all_dates' (execute)
-
 with
 days as (
     {{ 
