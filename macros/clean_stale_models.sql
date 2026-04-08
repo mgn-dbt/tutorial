a5b6712b1c7c31425c399f779d1364162a0ac4c7
GUEDON Marc <marc.guedon@cgi.com>
2026-04-08 17:51:06 +0200

    clean_stale debug

--- macros/clean_stale_models.sql
+++ macros/clean_stale_models.sql
@@ -11,21 +11,21 @@
 {% macro clean_stale_models(database=target.database, schema=target.schema, days=7, dry_run=True) %}
     
     {% set get_drop_commands_query %}
-        select
-            case 
+        select 
+            'DROP ' || case 
                 when table_type = 'VIEW'
                     then table_type
                 else 
                     'TABLE'
-            end as drop_type, 
-            'DROP ' || drop_type || ' {{ database | upper }}.' || table_schema || '.' || table_name || ';'
-        from {{ database }}.information_schema.tables 
-        where table_schema = upper('{{ schema }}')
-        and last_altered <= current_date - {{ days }} 
+            end || ' {{ database | upper }}.' || table_schema || '.' || table_name || ';'
+        from {{ database }}.{{ schema }}.INFORMATION_SCHEMA.TABLES 
+        where 1=1
+        --and table_schema = upper('{{ schema }}')
+        and DATE(creation_time) <= current_date - {{ days }}
     {% endset %}
 
     {{ log('\nGenerating cleanup queries...\n', info=True) }}
-    {% set drop_queries = run_query(get_drop_commands_query).columns[1].values() %}
+    {% set drop_queries = run_query(get_drop_commands_query).columns[0].values() %}
 
     {% for query in drop_queries %}
         {% if dry_run %}
@@ -33,7 +33,7 @@
         {% else %}
             {{ log('Dropping object with command: ' ~ query, info=True) }}
             {% do run_query(query) %} 
-        {% endif %}       
+        {% endif %}
     {% endfor %}
-    
+
 {% endmacro %}
