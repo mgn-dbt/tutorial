2e3911fdce5f87701b58c755d9d7f7b6a75030dd
GUEDON Marc <marc.guedon@cgi.com>
2026-04-14 17:02:53 +0200

    formatage

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,3 +1,6 @@
+{#
+special treatment for dev environment seeds
+#}
 {% macro generate_schema_name(custom_schema_name, node) %}
 
     {%- set default_schema = target.schema -%}
@@ -5,7 +8,6 @@
     {%- if custom_schema_name is none -%}
         {{ default_schema }}
     {%- else -%}
-        {#  special treatment for dev environment seeds #}
         {% if node.resource_type == 'seed' and env_var('DBT_ENV_NAME', 'sans') == 'dev' %}
             {{ custom_schema_name | trim }}
         {% else %}
--- macros/mockable_source.sql
+++ macros/mockable_source.sql
@@ -1,11 +1,12 @@
 
-{% macro mockable_source(source_name, table_name, sample_table_name) %}
+{% macro mockable_source(source_name, table_name, mock_table_name) %}
 
     {%- set target_name = target.name.lower() -%}
 
-    {% if target_name in ['ci','default', 'dev'] %}
-        {{ ref(sample_table_name) }}
+    {% if target_name in ['ci', 'default', 'dev'] %}
+        {{ ref(mock_table_name) }}
     {% else %}
         {{ source(source_name, table_name) }}
     {% endif %}
+
 {% endmacro %}
