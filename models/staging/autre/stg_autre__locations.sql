114ccb14647460b0bc9dada091feeeadb22a4cf2
GUEDON Marc <marc.guedon@cgi.com>
2026-04-21 17:28:26 +0200

    cherry pick dev

--- models/staging/autre/stg_autre__locations.sql
+++ models/staging/autre/stg_autre__locations.sql
@@ -9,17 +9,20 @@ renamed as (
         id as location_id,
         ---------- properties
         name as location_name,
-        {{ dbt.cast('tax_rate', dbt.type_float()) }} as tax_rate,
+        cast(tax_rate as {{ dbt.type_float() }}) as tax_rate,
         ---------- timestamp
-        {%- set dt_regex -%}
-            regexp_replace({{ dbt.cast('opened_at', dbt.type_string()) }},
-            '^(\\d{4}-\\d{2}-\\d{2}).*',
-            '\\1')
-        {%- endset -%}
-        {{ dbt.cast(dt_regex, 'date') }} as opened_at
+        cast({{ my_regexp_replace(
+            'cast(opened_at as ' ~ dbt.type_string() ~ ')',
+            '^(\d{4}-\d{2}-\d{2}).*',
+            '\1') }} as date) as opened_at
     from source
 )
 
 select * from renamed
 where
-    opened_at <= {{ ensure_bq_date(var('truncate_timespan_to')) }}
+{# bigquery date comparison doesn't accept type mismatch date/timestamp #}
+    opened_at <= {% if target.type == 'bigquery' -%}
+        DATE({{ var('truncate_timespan_to') }})
+    {%- else -%}
+        {{ var('truncate_timespan_to') }}
+    {%- endif %}
--- models/staging/autre/stg_autre__orders.sql
+++ models/staging/autre/stg_autre__orders.sql
@@ -13,14 +13,11 @@ renamed as (
         {{ cents_to_dollars('subtotal') }} as subtotal,
         {{ cents_to_dollars('tax_paid') }} as tax_paid,
         {{ cents_to_dollars('order_total') }} as order_total,
-
         ---------- timestamps
-        {%- set dt_regex -%}
-            regexp_replace({{ dbt.cast('ordered_at', dbt.type_string()) }},
-            '^(\\d{4}-\\d{2}-\\d{2})\\s(\\d{2}:\\d{2}:\\d{2}).*',
-            '\\1T\\2')
-        {%- endset -%}
-        {{ dbt.cast(dt_regex, dbt.type_timestamp()) }} as ordered_at
+        cast({{ my_regexp_replace(
+            'cast(ordered_at as ' ~ dbt.type_string() ~ ')',
+            '^(\d{4}-\d{2}-\d{2})\s(\d{2}:\d{2}:\d{2}).*',
+            '\1T\2') }} as {{ dbt.type_timestamp() }}) as ordered_at
     from source
 )
 
