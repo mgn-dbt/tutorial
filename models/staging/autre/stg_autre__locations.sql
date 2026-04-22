3ff27e17885d81b9043159426ca5ab9882179f4f
GUEDON Marc <marc.guedon@cgi.com>
2026-04-22 16:58:39 +0200

    pb regexp

--- models/staging/autre/stg_autre__locations.sql
+++ models/staging/autre/stg_autre__locations.sql
@@ -11,18 +11,19 @@ renamed as (
         name as location_name,
         cast(tax_rate as {{ dbt.type_float() }}) as tax_rate,
         ---------- timestamp
-        cast({{ my_regexp_replace(
-            'cast(opened_at as ' ~ dbt.type_string() ~ ')',
-            '^(\d{4}-\d{2}-\d{2}).*',
-            '\1') }} as date) as opened_at
+        cast(
+            regexp_replace(
+                cast(opened_at as {{ dbt.type_string() }}),
+                {%- if target.type == 'bigquery' -%}{# bigquery raw text #}
+                r{%- endif %}'^(\d{4}-\d{2}-\d{2}).*',
+                '\\1')
+            as date) as opened_at
     from source
 )
 
 select * from renamed
 where
-{# bigquery date comparison doesn't accept type mismatch date/timestamp #}
     opened_at <= {% if target.type == 'bigquery' -%}
-        DATE({{ var('truncate_timespan_to') }})
-    {%- else -%}
-        {{ var('truncate_timespan_to') }}
-    {%- endif %}
+        {# bigquery comparison doesnt accept type mismatch date/timestamp #}
+        DATE({{ var('truncate_timespan_to') }}){%- else -%}
+        {{ var('truncate_timespan_to') }}{%- endif %}
