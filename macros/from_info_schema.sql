992a8324f8c3d09ab92816de0288abe7a9532339
GUEDON Marc <marc.guedon@cgi.com>
2026-04-23 17:25:56 +0200

    US for default location

--- macros/from_info_schema.sql
+++ macros/from_info_schema.sql
@@ -1,13 +1,18 @@
 {#
 https://github.com/bbrewington/dbt-bigquery-information-schema/blob/main/dbt_bigquery_info_schema/macros
+if in dbt cloud we don't have the gcp location so we default to region-US
 #}
 
 {%- macro from_info_schema(info_schema_type, dataset) -%}
 {% if target.type == 'bigquery' -%}
     {%- if dataset -%}
-    `{{ target.database }}.{{ dataset }}`.INFORMATION_SCHEMA.{{ info_schema_type|upper }}
+        `{{ target.database }}.{{ dataset }}`.INFORMATION_SCHEMA.{{ info_schema_type|upper }}
     {%- else -%}
-    `{{ target.database }}.region-{{ target.location|upper }}`.INFORMATION_SCHEMA.{{ info_schema_type|upper }}
+        {% if target.location is none %}
+            `{{ target.database }}.region-US`.INFORMATION_SCHEMA.{{ info_schema_type|upper }}
+        {% else %}
+            `{{ target.database }}.region-{{ target.location|upper }}`.INFORMATION_SCHEMA.{{ info_schema_type|upper }}
+        {%- endif -%}
     {%- endif -%}
 {% elif target.type == 'duckdb' %}
     "system".information_schema.{{ info_schema_type|lower }}
